8f06a1ffe5c74ce3a60434922758fa5ef0850208
hthang <hanghoangmmt97@gmail.com>
2020-04-21 17:39:53 +0700

    cmt

--- assignment_1/script.js
+++ assignment_1/script.js
@@ -70,7 +70,8 @@ function validate(id) {
 
 function add() {
     var newId = this.newId();
-    var textInput = '<input onblur="validate(' + newId + ')" validate id="' + newId + '" class="input-value" type="text">';
+    // onblur="validate(' + newId + ')" validate id="' + newId + '"
+    var textInput = '<input  class="input-value" type="text">';
     var removeBtn = '<button onclick="remove(' + newId + ')" class="input-btn"><i class="fa fa-close"></i></button>';
     var html = '<div class="input-sub">' + textInput + removeBtn + '</div>'
 
@@ -115,4 +116,59 @@ function getInputValues() {
     }
     console.log(response);
     return response;
+}
+
+function addImg() {
+    var newId = this.newId();
+    var img = '<img src="#" id ="img' + newId + '" class ="input-value-file">';
+    var inputImg = '<input type="file" id="filetag" name="filetag" onchange="changeimage(this,' + "'img" + newId + "'" + ')"><br><br>';
+    var deleteImg = '<button onclick="removeImg('+ newId +')" class="input-btn"><i class="fa fa-close"></i></button>';
+    var deltePre = '<button onclick =removePre(' + "'img" + newId + "'" + ')>Delete Preview</button>';
+    var html = '<div class ="input-file-sub">' + img + inputImg + deleteImg + deltePre +  '</div>';
+
+    var newElement = document.createElement('div');
+    newElement.innerHTML = html;
+    newElement.id = newId;
+    var currentElement = document.getElementById("input-file-wrapper");
+    currentElement.append(newElement);
+    console.log(newId);
+}
+
+function changeimage(input, id) {
+    var reader;
+    // var fileTag = document.getElementById("filetag"),
+    var imageTag = document.getElementById(id);
+    console.log(imageTag);
+    if (input.files && input.files[0]) {
+        reader = new FileReader();
+
+        reader.onload = function (e) {
+            imageTag.setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function removeAll(){
+    var currentElement = document.getElementById("input-file-wrapper");
+    if(currentElement) currentElement.innerHTML ="";
+}
+
+function removeImg(id){
+    debugger
+    var currentElement = document.getElementById(id);
+    if(currentElement) currentElement.remove();
+}
+function removePre(id){
+    debugger
+    var currentElement = document.getElementById(id)
+    if(currentElement) currentElement.src = "";
+}
+function clearInput(){
+    debugger
+    var elements = document.getElementsByClassName('input-value-file');
+    if (!elements || elements.length == 0) return;
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].src ="";
+    }
 }
